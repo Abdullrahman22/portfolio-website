a155a2118da394f3424df45dbc7c7145e4c99a71
Abdullrahman22 <abdullrahman.ismael22@gmail.com>
2020-10-07 18:43:52 +0200

    Finish Login As Admin

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Models\Messege;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
@@ -17,27 +17,29 @@ class AuthController extends Controller
      public function login(Request $request){
 
           $rules = [
-               "username" =>  "required | min:8 | max:50 ",
+               "username" =>  "required | min:4 | max:50 ",
                "password" =>  "required | min:8 | max:50 " ,
           ];
 
           $validator = Validator::make(  $request->all() , $rules  ); 
 
-          if( $validator -> fails() ) { 
+          if( $validator -> fails() ) {   // if validation Failed
                return redirect() -> back() ->withErrors($validator)->withInput();
           }
 
-          // if(   
-          //      Auth::guard("admin") -> attempt([  // attempt ==> mean see it in tables
-          //           "email" => $request->email ,
-          //           "password" => $request->password
-          //      ])  
-          // ){  
-          //      return redirect() -> to("/admin/overview");
-          // }else{
-          //      return redirect() -> back() ->withErrors( $validator->getMessageBag()->add('email', " your admin data ins't correct ") )->withInput();
-          // }
+          if(        
+               Auth::guard("admin") -> attempt([  // attempt ==> mean see it in tables
+                    "username" => $request->username ,
+                    "password" => $request->password
+               ])  
+          ){  
+               return redirect() -> to("/admin");  // recodes the same with requests create session guard admin
+          }else{
+               // if validate success but no same in db recordes ==> return back login page with custom error
+               return redirect() -> back() ->withErrors( $validator->getMessageBag()->add('username', " your admin data ins't correct ") )->withInput();
+          }
 
      }
+
 
 }
--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
-class Admin extends Model
+use Illuminate\Foundation\Auth\User as Authenticatable;
+class Admin extends Authenticatable  // to use attepm func for auth
 {
     protected $fillable  = [ "username" , "email" , "password" ];
     protected $hidden    = [ "created_at" , "updated_at" ] ;
